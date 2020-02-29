7dbc9eb4adcf205eb931d0420948447b40140ff6
Adrian <Zilibdev@gmail.com>
2020-03-01 00:59:10 +0100

    Readme, homepage baner view, add sass, first commit. Basic commit.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Blog
-Everything is included in the readme
+## Assumptions 
+**The application**
+* Is created in the .net core 3.1, using model-view-controller,  
+* Using Jquery,
+* Will be have simple CRUD system,
+* No CSS frameworks, vanilla css. I will only use sass preprocessor,  
+* Education destination,
+* **Entity Framework!**,
+* Blog is about poems, i will put there some my own poems :)
---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(() => {
+    // Check does element exist
+    if ($("Header").length) {
+        // Animate it, by css.
+        if ($("Header").hasClass("Invisible")) { // Check does header exist
+            $("Header").toggleClass("Invisible"); // if exist, remove class for animation object
+        }
+    }
+});
