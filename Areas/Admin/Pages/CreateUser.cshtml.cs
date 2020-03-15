5e361e626294c6dbad7e0a5bba9715f2404b0c56
Adrian <Zilibdev@gmail.com>
2020-03-15 12:53:01 +0100

    Create new user from admin panel, in the future only administrator will be able to register a new user.

--- Areas/Admin/Pages/CreateUser.cshtml.cs
+++ Areas/Admin/Pages/CreateUser.cshtml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Blog.Areas.Data;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,11 +18,13 @@ namespace Blog.Areas.Admin.Pages
 
         private readonly UserManager<BlogUser> _userManager;
         private readonly ILogger<LoginModel> _logger;
+        private readonly SignInManager<BlogUser> _signInManager;
 
         #endregion
 
         #region Public variables 
         
+        [BindProperty]
         public InputModel Input { get; set; }
 
         #endregion
@@ -29,11 +32,13 @@ namespace Blog.Areas.Admin.Pages
         #region Construct
         public CreateUserModel(
             ILogger<LoginModel> logger,
-            UserManager<BlogUser> userManager
+            UserManager<BlogUser> userManager,
+            SignInManager<BlogUser> signInManager
             )
         {
             _userManager = userManager;
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         #endregion
@@ -55,32 +60,33 @@ namespace Blog.Areas.Admin.Pages
         public class InputModel
         {
             #region Required
-
-            [Required]
-            [Display(Name = "Nazwa u¿ytkownika")]
-            public string NickName { get; set; }
-
-            [Required]
-            [DataType(DataType.EmailAddress)]
+            [Required(ErrorMessage = "Pole '{0}' jest wymagane!")]
+            [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Pole '{0}' jest wymagane!")]
             [DataType(DataType.Password)]
             [Display(Name = "Has³o")]
             public string Password { get; set; }
 
             [Required]
+            [Display(Name = "PotwierdŸ has³o")]
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "Has³a do siebie nie pasuj¹")]
+            public string ConfirmPassword { get; set; }
+
+            [Required(ErrorMessage = "Pole '{0}' jest wymagane!")]
             [Display(Name = "Imiê")]
             public string Name { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Pole '{0}' jest wymagane!")]
             [Display(Name = "Nazwisko")]
             public string Surname { get; set; }
 
-            [Required]
+            [Required(ErrorMessage = "Pole '{0}' jest wymagane!")]
             [Display(Name = "Data urodzenia")]
-            [DataType(DataType.Date)]
+            [DataType(DataType.Date, ErrorMessage = "Wartoœæ jest nieprawid³owa")]
             public DateTime BirthDate { get; set; }
 
             #endregion
@@ -101,6 +107,46 @@ namespace Blog.Areas.Admin.Pages
             }
 
             SetUserData(user);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if (user == null)
+            {
+                return RedirectToPage("/Login", new { area = "Admin" });
+            }
+
+            if (ModelState.IsValid)
+            {
+                var newUser = new BlogUser
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    Name = Input.Name,
+                    Surname = Input.Surname,
+                    BirthDate = Input.BirthDate,
+                    PhoneNumber = Input.MobilePhone
+                };
+
+                var result = await _userManager.CreateAsync(newUser, Input.Password);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Wszystko okej, uzytkownik zarejestrowany");
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(newUser);
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = newUser.Id, code = code},
+                        protocol: Request.Scheme);
+
+                    
+                }
+                return Page();
+            }
 
             return Page();
         }
